705172a03b33d26fcde0a7427cffd727a4787dce
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-05-28 23:49:17 +0200

    fucked up, should have made front-end vanilla...

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -1,28 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var cors = require('cors');
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const usersDir = path.join(__dirname, '../data/Users.json')
 
 router.use(cors());
 
-router.get('/', function(req, res, next) {
-  res.send('<h1>respond with a users resource</h1>');
+router.get('/', (req, res, next) => {
+  console.log(usersDir)
+
+  fs.readFile(usersDir, (err, data) => {
+    if (err) throw err;
+
+    let users = JSON.parse(data);
+    console.log(users)
+
+    res.send(users);
+  })
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   res.send(`<h1>You are visiting user id: ${req.params.id}</h1>`);
 });
 
-router.post('/', (req,res)=>{
+router.post('/', (req, res, next) => {
   console.log(req.body)
   res.send(`Received a post with the following body: ...`);
 });
 
-router.put('/:id', (req,res)=>{
+router.put('/:id', (req, res) => {
   console.log(req.params.id)
   return res.send(`Received a put on ID: ${req.params.id}`);
 });
 
-router.delete('/api/users/:id', (req,res)=>{
+router.delete('/api/users/:id', (req, res) => {
   console.log(req.params.id)
   return res.send(`Received a delete on ID: ${req.params.id}`);
 });
