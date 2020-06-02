154068039b12b881eff5445d0a601bd3899641f8
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-06-02 19:42:52 +0200

    0.1

---
+++ src/frontend/newsletter/src/components/NewsletterSite.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function NewsLetterSite() {
+    return (
+        <h1>NewsLetterSite</h1>
+    );
+}
+export default NewsLetterSite;
---
+++ src/frontend/newsletter/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import NewsletterSite from './components/NewsletterSite';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NewsletterSite />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
